a85b07ac6b8a23fa21e9f773c8acbb10b15f4e47
Mohammad Edrees <edrees_saadat@yahoo.com>
2021-01-31 23:31:57 -0500

    Initial Commit

---
+++ SwiftyUiLabel/SwiftyUiLabel.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyUiLabel.h
+//  SwiftyUiLabel
+//
+//  Created by Mohammad Edrees on 1/31/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyUiLabel.
+FOUNDATION_EXPORT double SwiftyUiLabelVersionNumber;
+
+//! Project version string for SwiftyUiLabel.
+FOUNDATION_EXPORT const unsigned char SwiftyUiLabelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUiLabel/PublicHeader.h>
+
+
